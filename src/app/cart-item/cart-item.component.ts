02f74dc2e4a5e84e364373eba369acf18ffef9cf
dasith <dasiththisaru@gmail.com>
2022-02-03 18:16:46 +0530

    Update cart-item template

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Item} from "../dto/item";
 
 @Component({
   selector: 'app-cart-item',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-item.component.scss']
 })
 export class CartItemComponent implements OnInit {
+
+  item = new Item("I001",
+    "https://m.media-amazon.com/images/I/71NTi82uBEL._AC_UL320_.jpg",
+    "Apple AirPods with Charging Case",
+    4,
+    299,
+    5);
 
   constructor() { }
 
