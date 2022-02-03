863f320928fa37620b139cf96f95a70cacb807d3
dasith <dasiththisaru@gmail.com>
2022-02-03 17:56:04 +0530

    Add item dto fields

--- src/app/dto/item.ts
+++ src/app/dto/item.ts
@@ -1,2 +1,10 @@
 export class Item {
+
+  constructor(public code: string,
+              public image: string,
+              public description: string,
+              public rating: 1 | 2 | 3 | 4 | 5,
+              public price: number,
+              public qty: number) {
+  }
 }
