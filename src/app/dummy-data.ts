ab400f9a9c2c6ac164c3b00a526a1a5164923839
dasith <dasiththisaru@gmail.com>
2022-02-05 05:49:53 +0530

    Add some dummy data

--- src/app/dummy-data.ts
+++ src/app/dummy-data.ts
@@ -1,7 +1,44 @@
 import {Item} from "./dto/item";
 
 
- export const DUMMY_DATA : Array<Item> = {
+ export const DUMMY_DATA : Array<Item> = [
+   new Item("I001",
+     "https://m.media-amazon.com/images/I/71NTi82uBEL._AC_UL320_.jpg",
+     "Apple AirPods with Charging Case",
+     3,
+     299,
+     5),
+   new Item("I002",
+     "https://m.media-amazon.com/images/I/81Sxdp0JBLL._AC_UL320_.jpgg",
+     "Nintendo Switch - Animal Crossing: New Horizons Edition - Switch",
+     4,
+     55,
+     8),
+   new Item("I003",
+     "https://m.media-amazon.com/images/I/61i421VnFYL._AC_UL320_.jpg",
+     "Nintendo Switch with Gray Joy‑Con - HAC-001(-01)",
+     5,
+     55,
+     4),
+   new Item("I004",
+     "https://m.media-amazon.com/images/I/71qodgwQQ7S._AC_UL320_.jpg",
+     "Acer Aspire 5 A515-56-36UT Slim Laptop | 15.6\" Full HD Display | 11th Gen Intel Core i3-1115G4",
+     5,
+     413.99,
+     10),
+   new Item("I005",
+     "https://m.media-amazon.com/images/I/61O9tWR6WDS._AC_UL320_.jpg",
+     "PlayStation DualSense Wireless Controller – Midnight Black",
+     4,
+     69,
+     8),
+   new Item("I006",
+     "https://m.media-amazon.com/images/I/51pbZ+OFuCS._AC_UY218_.jpg",
+     "Madden NFL 22 - PlayStation 4",
+     3,
+     39.99,
+     8)
+ ]
 
 
-}
+
