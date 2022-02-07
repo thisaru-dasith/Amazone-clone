d3258b3059831012642af86b0677bb29aa6235d4
dasith <dasiththisaru@gmail.com>
2022-02-07 11:11:31 +0530

    Revamp with CartService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,28 +9,5 @@ import {Item} from "./dto/item";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  item = DUMMY_DATA;
-  cartItem : Array<{code:String, qty : number}> = []
 
-  updateCart(inCart: number, it: Item) {
-    const item = this.cartItem.find(i => i.code === it.code);
-
-    if (item){
-      item.qty = inCart;
-
-      if (item.qty === 0){
-        this.cartItem.splice(this.cartItem.indexOf(item), 1);
-      }
-    }else{
-      this.cartItem.push({code: it.code, qty: inCart});
-    }
-  }
-
-
-  getTotalItemsInCart(): number{
-    let totalItems = 0;
-
-    this.cartItem.forEach(item => totalItems += item.qty);
-    return totalItems;
-  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import {CartService} from "../service/cart.service";
 })
 export class HeaderComponent implements OnInit {
 
-  @Input()
+
   totalItemsInCart = 0;
 
   constructor(private cartService : CartService) { }
