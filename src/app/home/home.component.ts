815e5d1d5dfd0de5713363e0901ece530d2e68dd
dasith <dasiththisaru@gmail.com>
2022-02-07 10:36:56 +0530

    Update the CartService with updateCart method

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,19 +17,7 @@ export class HomeComponent implements OnInit {
     this.item = itemService.getAllItem()
   }
 
-  /*updateCart(inCart: number, it: Item) {
-    const item = this.cartItem.find(i => i.code === it.code);
 
-    if (item){
-      item.qty = inCart;
-
-      if (item.qty === 0){
-        this.cartItem.splice(this.cartItem.indexOf(item), 1);
-      }
-    }else{
-      this.cartItem.push({code: it.code, qty: inCart});
-    }
-  }*/
   ngOnInit(): void {
   }
 
--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {Item} from "../dto/item";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  cartItem : Array<{code:String, qty : number}> = []
+
   constructor() { }
+
+  updateCart(it: Item,toCart: number) {
+   const item = this.cartItem.find(i => i.code === it.code);
+
+   if (item){
+     item.qty = toCart;
+
+     if (item.qty === 0){
+       this.cartItem.splice(this.cartItem.indexOf(item), 1);
+     }
+   }else{
+     this.cartItem.push({code: it.code, qty: toCart});
+   }
+ }
 }
