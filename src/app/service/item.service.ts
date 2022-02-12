943bb41712fcfa4f709f3abe69d977200b5bb9ac
dasith <dasiththisaru@gmail.com>
2022-02-12 10:58:05 +0530

    Extract the logic of map function to a separate function

--- src/app/service/item.service.ts
+++ src/app/service/item.service.ts
@@ -15,37 +15,35 @@ export class ItemService {
 
   getAllItems() : Observable<Array<Item>>{
     return this.http.get<Array<Item>>(this.ITEM_SERVICE_API).pipe(
-      map(items => {
-        const tempItems: Array<Item> = [];
-
-        items.forEach(item => {
-          switch (item.rating as any){
-            case "SECOND":
-              item.rating = 2;
-              break;
-            case "THIRD":
-              item.rating = 3;
-              break;
-            case "FOURTH":
-              item.rating = 4;
-              break;
-            case "FIFTH":
-              item.rating = 5;
-              break;
-            default:
-              item.rating = 1;
-          }
-
-          item.price = (item as any).unitPrice;
-          tempItems.push(item);
-        });
-
-        return tempItems;
-      }));
+      map(items => items.map(convertToItem)));
   }
 
   getItem(code: string): Observable<Item> {
-    return this.http.get<Item>(`${this.ITEM_SERVICE_API}?code=${code}`);
+    return this.http.get<Item>(`${this.ITEM_SERVICE_API}?code=${code}`).pipe(
+      map(convertToItem)
+    );
   }
 
+}
+
+function convertToItem(item: any): Item{
+  switch (item.rating as any){
+    case "SECOND":
+      item.rating = 2;
+      break;
+    case "THIRD":
+      item.rating = 3;
+      break;
+    case "FOURTH":
+      item.rating = 4;
+      break;
+    case "FIFTH":
+      item.rating = 5;
+      break;
+    default:
+      item.rating = 1;
+  }
+
+  item.price = (item as any).unitPrice;
+  return item;
 }
